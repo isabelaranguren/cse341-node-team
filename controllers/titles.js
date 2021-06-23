21c484f1ada7666e7f8e4176125680ae317c40bb
Adrian Leal <lea19019@byui.edu>
2021-06-22 21:38:02 -0500

    index work

--- controllers/titles.js
+++ controllers/titles.js
@@ -2,30 +2,24 @@ const titles = require('../models/titles');
 const Title = require('../models/titles');
 const fetch = require('node-fetch');
 
-
 // https://api.themoviedb.org/3/movie/popular?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US&page=1
 
 // https://api.themoviedb.org/3/movie/top_rated?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
 
 // https://api.themoviedb.org/3/movie/upcoming?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
 
-
 // Tv
 // https://api.themoviedb.org/3/tv/latest?api_key=f4278fc5b9413965242b5e22893f273&language=en-US
 // https://api.themoviedb.org/3/tv/popular?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
 // https://api.themoviedb.org/3/tv/top_rated?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
 
-
 // https://api.themoviedb.org/3/watch/providers/regions?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US
-
 
 exports.getIndex = (req, res, next) => {
     const page = req.params.page || 1;
     const offset = 10 * (page - 1);
-    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US&page=${page}`)
-
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US&page=${page}`)
         // https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
-
         .then(response => response.json())
         .then(titles => {
             console.log(titles.results[0])
