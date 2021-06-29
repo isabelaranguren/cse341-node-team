6d46e60b89fe9cf66a63cb8af2dbc0d0cfd1ddec
Adrian Leal <lea19019@byui.edu>
2021-06-28 19:31:10 -0500

    signup user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,17 @@
-const User = require('../models/user');
-
 const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+const { validationResult } = require('express-validator/check')
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
     if (message.length > 0) {
-        message = message[0];
-    } else {
-        message = null;
+        message = message[0]
     }
-
-    res.render('auth/login', {
+    else {
+        message = null
+    }
+    res.render('pages/auth/login', {
         path: '/login',
         pageTitle: 'Login',
         errorMessage: message,
@@ -18,21 +19,140 @@ exports.getLogin = (req, res, next) => {
             email: '',
             password: ''
         },
+        validationErrors: []
+    });
+};
+
+exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0]
+    }
+    else {
+        message = null
+    }
+    res.render('pages/auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validationErrors: []
     });
 };
 
 exports.postLogin = (req, res, next) => {
-
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('pages/auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password
+            },
+            validationErrors: errors.array()
+        });
+    }
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                return res.status(422).render('pages/auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password.',
+                    oldInput: {
+                        email: email,
+                        password: password
+                    },
+                    validationErrors: []
+                });
+            }
+            bcrypt
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save((err) => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    return res.status(422).render('auth/login', {
+                        path: '/login',
+                        pageTitle: 'Login',
+                        errorMessage: errors.array()[0].msg,
+                        oldInput: {
+                            email: email,
+                            password: password
+                        },
+                        validationErrors: []
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httStatusCode = 500;
+            return next(error);
+        });
 };
 
-exports.getSignup = (req, res, next) => {
-
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
 
 exports.postSignup = (req, res, next) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const userName = req.body.userName;
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = validationResult(req);
 
-};
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(422).render('pages/auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg,
+            oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword },
+            validationErrors: errors.array()
+        });
 
-// exports.postDeleteAccount = (req,res,next) => {
+    }
 
-// };
+    bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                firstName: firstName,
+                lastName: lastName,
+                userName: userName,
+                email: email,
+                password: hashedPassword
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+            return;
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httStatusCode = 500;
+            return next(error);
+        });
+}
