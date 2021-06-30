743498ccb67b86cba23696422683e6ed4ff4740e
Spencer <spencer@foo.com>
2021-06-30 14:42:27 -0600

    minor changes

--- controllers/titles.js
+++ controllers/titles.js
@@ -2,6 +2,7 @@ const titles = require('../models/titles');
 const Title = require('../models/titles');
 const fetch = require('node-fetch');
 
+
 // https://api.themoviedb.org/3/movie/popular?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US&page=1
 
 // https://api.themoviedb.org/3/movie/top_rated?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
@@ -55,8 +56,32 @@ exports.getTopRated = (req, res, next) => {
 };
 
 exports.getMylist = (req, res, next) => {
+    res.render('userList')('pages/userList', {
+        path:'',
+        
+
+    });
 
 };
+
+// exports.getCart = (req, res, next) => {
+//     req.user
+//       .populate('cart.items.productId')
+//       .execPopulate()
+//       .then(user => {
+//         const products = user.cart.items;
+//         res.render('shop/cart', {
+//           path: '/cart',
+//           pageTitle: 'Your Cart',
+//           products: products,
+//         });
+//       })
+//       .catch(err => {
+//         const error = new Error(err);
+//         error.httpStatusCode = 500;
+//         return next(error);
+//       });
+//   };
 
 exports.postDeleteList = (req, res, next) => {
 
@@ -67,17 +92,6 @@ exports.postTitle = (req, res, next) => {
 };
 
 exports.getTitleDetails = (req, res, next) => {
-    // get movie id from the URL. Im not sure how to do
-    // this without the URL saying ?id=####### but once
-    // the id is found check the database for a match
-    // to see if the movie is already in the database
-    // if not then add it. Idk when we would use the 
-    // id provided to us by mongodb (_id). Maybe we
-    // could use that somehow. Then use that id to
-    // fetch the rest of the data about the movie
-    // that we want to display such as title and 
-    // description and then have a button that 
-    //adds that movie to the user's movie list.
     const titleId = req.params.id;
     //const imdb_id = req.body.imdb_id;
     fetch(`https://api.themoviedb.org/3/movie/${titleId}?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US`)
@@ -118,12 +132,7 @@ exports.getTitleDetails = (req, res, next) => {
         .catch(err => {
             console.log(err);
         })
-    // res.render('pages/mediaDetails', {
-    //     titleId: titleId,
-    //     path: '/title/:titleId',
-    //     pageTitle: 'Movie Details'
-
-    // });
+    
 
 };
 
