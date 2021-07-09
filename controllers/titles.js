889c838a51772d79154f538fb5b8af2f16da9f5a
isabelaranguren <60532846+isabelaranguren@users.noreply.github.com>
2021-07-08 20:02:45 -0500

    Home

--- controllers/titles.js
+++ controllers/titles.js
@@ -2,19 +2,6 @@ const titles = require('../models/titles');
 const Title = require('../models/titles');
 const fetch = require('node-fetch');
 
-// https://api.themoviedb.org/3/movie/popular?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US&page=1
-
-// https://api.themoviedb.org/3/movie/top_rated?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
-
-// https://api.themoviedb.org/3/movie/upcoming?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
-
-// Tv
-// https://api.themoviedb.org/3/tv/latest?api_key=f4278fc5b9413965242b5e22893f273&language=en-US
-// https://api.themoviedb.org/3/tv/popular?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
-// https://api.themoviedb.org/3/tv/top_rated?api_key=f4278fc5b9413965242b5e22893f273&language=en-US&page=1
-
-// https://api.themoviedb.org/3/watch/providers/regions?api_key=f4278fc5b9413965242b5e22893f2738&language=en-US
-
 exports.getIndex = (req, res, next) => {
     const page = +req.query.page || 1;
     const offset = 10 * (page - 1);
@@ -89,6 +76,8 @@ exports.postDeleteList = (req, res, next) => {
 exports.postTitle = (req, res, next) => {
 
 };
+
+
 
 exports.getTitleDetails = (req, res, next) => {
     const titleId = req.params.id;
